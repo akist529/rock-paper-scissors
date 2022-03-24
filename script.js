979b2af2d70dc1fc5fa1610c98f60c4d5677402e
Alex Kist <akist0707@gmail.com>
2022-03-24 18:24:33 -0500

    Add playRound() function

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function computerPlay() {
 
     return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Tie!';
+    }
+
+    let playerLose = `You lose! ${computerSelection} beats ${playerSelection}`;
+    let playerWin = `You win! ${playerSelection} beats ${computerSelection}`;
+
+    switch (playerSelection) {
+        case 'Rock':
+            return (computerSelection === 'Paper') ? playerLose : playerWin;
+        case 'Paper':
+            return (computerSelection === 'Scissors') ? playerLose: playerWin;
+        case 'Scissors':
+            return (computerSelection === 'Rock') ? playerLose: playerWin;
+    }
+}
